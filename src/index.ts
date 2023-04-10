eca5fea3635c96992d5412820f1b1ada228451a4
Gabriel Garuthi <gabrieldrmg@hotmail.com>
2023-04-10 10:42:55 -0300

    Atualizado

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { db } from './database/knex'
 
 const app = express()
 
@@ -25,5 +26,59 @@ app.get("/ping", async (req: Request, res: Response) => {
         } else {
             res.send("Erro inesperado")
         }
+    }
+})
+
+app.get("/bands", async (req: Request, res: Response) => {
+    try {
+        const result = await db.raw(`SELECT * FROM bands;`)
+        res.status(200).send(result)
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+app.post("/bands", async (req: Request, res: Response) => {
+    try {
+            const id = req.body.id
+            const name = req.body.name
+            if(typeof id !== "string"){
+                res.status(400)
+                throw new Error("Id inválido - deve ser uma string")
+            }
+            if(typeof name !== "string"){
+                res.status(400)
+                throw new Error("Name inválido - deve ser uma string")
+            }
+
+            if(!id.length){
+                res.status(400)
+                throw new Error("'Id' inválido - id não pode ser vazio")
+            }
+            await db.raw(`INSERT INTO bands(id, name) VALUES ("${id}", "${name}");`)
+            res.status(201).send("Banda criada com sucesso!")
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+        
     }
 })
