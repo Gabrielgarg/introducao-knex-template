8ea6117b2d319eb3a370440747426ae0d9c2ffe0
Gabriel Garuthi <gabrieldrmg@hotmail.com>
2023-04-10 11:37:18 -0300

    Atualizado

--- src/index.ts
+++ src/index.ts
@@ -82,3 +82,64 @@ app.post("/bands", async (req: Request, res: Response) => {
         
     }
 })
+
+app.put("/bands/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+
+        const newid = req.body.id
+        const newname = req.body.name
+
+        if(id !== undefined && newid !== undefined){
+            if(typeof id !== "string"  && typeof newid !== "string"){
+                res.status(400)
+                throw new Error("Id inválido - deve ser uma string")
+            }
+            if(!id.length && !newid.length){
+                res.status(400)
+                throw new Error("'Id' inválido - id não pode ser vazio")
+            }
+
+        }
+
+        if(newname !== undefined){
+            if(typeof newname !== "string"){
+                res.status(400)
+                throw new Error("Name inválido - deve ser uma string")
+            }
+            if(!newname.length){
+                res.status(400)
+                throw new Error("'Id' inválido - id não pode ser vazio")
+            }
+        }
+
+        const [band] = await db.raw(`SELECT * FROM bands WHERE id = "${id}";`)
+
+        if(band){
+            await db.raw(`UPDATE bands 
+            SET 
+                id = "${newid  ||band.id}", 
+                name = "${newname || band.name}"
+            WHERE id = "${id}";`)
+            res.status(200).send("Banda atualizada com sucesso!")
+        }
+        else{
+            res.status(404).send("Banda não encontrada")
+        }
+
+
+
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
